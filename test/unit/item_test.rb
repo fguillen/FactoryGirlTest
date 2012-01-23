37d15cd7a6de29d32c3337a970d2da98be231062
Fernando Guillen <fguillen.mail@gmail.com>
2012-01-23 16:01:22 +0100

    testing 'format' attribute wihtout FactoryGirl

--- test/unit/item_test.rb
+++ test/unit/item_test.rb
@@ -5,4 +5,9 @@ class ItemTest < ActiveSupport::TestCase
     item = Factory(:item, :name => "the name", :format => "the format")
     assert_equal("the format", item.format)
   end
+
+  def test_format_without_factory_girl
+    item = Item.create!(:name => "the name", :format => "the format")
+    assert_equal("the format", item.format)
+  end
 end
